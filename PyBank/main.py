020233fede93c4cba6f74f95c10ba2cf08825a77
melisatahiraj <melisatahiraj00@gmail.com>
2023-11-06 18:27:41 -0800

    added files

---
+++ PyBank/main.py
@@ -0,0 +1,91 @@
+# Modules
+import os
+import csv
+import statistics as st
+
+budget_csv = os.path.join("Resources", "budget_data.csv")   
+
+# Set existing lists
+date = []
+pol = []
+
+# Create lists to store data
+total_months = []
+net_total = []
+average_change = []
+change_pol = []
+greatest_increase = []
+greatest_decrease = []
+
+# Set variables
+total_months = 0
+net_total = 0
+change = 0
+
+# Open and read csvfile:
+with open(budget_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter =',')
+
+    # Read the header
+    csv_header = next(csvfile)
+
+    # Read through each row of the data
+    for row in csvreader:
+
+        # Get the rows list data
+        date.append(row[0])
+        pol.append(row[1])
+
+        # Calculate total number of months
+        total_months = len(date)
+
+        # Calculate net total for Profits/Losses
+        net_total = net_total + int(row[1])
+
+    # Calculate average changes in Profits/Losses
+    for change in range(len(pol) - 1):
+        value_pol = int(pol[change + 1]) - int(pol[change])
+        change_pol.append(value_pol)
+    average_change = sum(change_pol)/len(change_pol)
+
+    # Calculate greatest increase in Profits (date and amount)
+    greatest_increase = max(change_pol)
+    greatest_increase_value = change_pol.index(greatest_increase)
+    greatest_increase_date = date[greatest_increase_value + 1]
+
+    # Calculate greatest decrease in Profits (date and amount)
+    greatest_decrease = min(change_pol)
+    greatest_decrease_value = change_pol.index(greatest_decrease)
+    greatest_decrease_date = date[greatest_decrease_value + 1]
+
+# Print Analysis Statements
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${net_total}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} (${str(greatest_increase)})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${str(greatest_decrease)})")
+
+
+# Set variable for output file
+output_file = os.path.join("Analysis_PyBank.txt")
+
+# Export to the output file
+with open(output_file, "w", newline='') as txtfile:
+    
+    # Print the output
+    output = (
+        f"Financial Analysis\n"
+        f"----------------------------\n"
+        f"Total Months: {total_months}\n"
+        f"Total: ${net_total}\n"
+        f"Average Change: ${average_change:.2f}\n"
+        f"Greatest Increase in Profits: {greatest_increase_date} (${str(greatest_increase)})\n"
+        f"Greatest Decrease in Profits: {greatest_decrease_date} (${str(greatest_decrease)})\n"
+    )
+
+    print(output)
+
+    # Export to Analysis text file
+    txtfile.write(output)
---
+++ PyPoll/main.py
@@ -0,0 +1,101 @@
+# Modules
+import os
+import csv
+
+election_csv = os.path.join("Resources", "election_data.csv")
+
+# Set existing lists
+ballot_id = []
+county = []
+candidate = []
+
+# Create variables to store data
+candidates_votes = {}
+candidates_list = []
+votes_total = []
+total_percent = []
+winner_candidate = " "
+
+# Set variables
+total_votes = 0
+winner_votes = 0
+
+# Open and read csvfile:
+with open(election_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter =',')
+
+    # Read the header
+    csv_header = next(csvfile)
+
+    # Read through each row of the data
+    for row in csvreader:
+
+        # Get the rows list data
+        ballot_id.append(row[0])
+        county.append(row[1])
+        candidate.append(row[2])
+
+        # Calulate total number of votes
+        total_votes += 1
+
+        candidate_name = row[2]
+
+        # Get the candidate name for each row
+        if candidate_name in candidates_list:
+            candidates_votes[candidate_name] = candidates_votes[candidate_name] + 1
+        else:
+            candidates_list.append(candidate_name)
+            candidates_votes[candidate_name] = 1
+
+    # Read through each candidates row of the data
+    for candidate_name, votes in candidates_votes.items():
+
+        votes_total.append(votes)
+
+        # Calculate total percentage of votes
+        votes_percent = round((votes / total_votes * 100), 3)
+        total_percent.append(votes_percent)
+
+        # Get the winners information
+        if votes > winner_votes:
+            winner_votes = votes
+            winner_candidate = candidate_name
+
+    # Create zipped list to organize variables
+    zipped_list = list(zip(candidates_list, total_percent, votes_total))
+
+
+# Set variable for output file
+output_file = os.path.join("Analysis_PyPoll.txt")
+
+# Export to the output file
+with open(output_file, "w", newline='') as txtfile:
+        
+    # Print the output
+    output = (
+        f"Election Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes}\n"
+        f"-------------------------\n"
+        f"{zipped_list[0][0]}: {zipped_list[0][1]}% ({zipped_list[0][2]})\n"
+        f"{zipped_list[1][0]}: {zipped_list[1][1]}% ({zipped_list[1][2]})\n"
+        f"{zipped_list[2][0]}: {zipped_list[2][1]}% ({zipped_list[2][2]})\n"
+        f"-------------------------\n"
+        f"Winner: {winner_candidate}\n"
+        f"-------------------------\n"
+    )
+    
+    print(output)
+
+    # Export to Analysis text file
+    txtfile.write(output)    
+
+
+
+
+
+
+
+
+
+        
